5892fdaa2a83e04dc3eaf800b9d0b21966b7ad86
adepusrikanth <sri.adepu007@gmail.com>
2020-05-13 14:16:07 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -13,5 +13,5 @@ def get_bot_response():
     userText = request.args.get('msg')
     return str(chatbot.get_response(userText))
 
-if __name__ == "__main__":
-    app.run() 
+if __name__== '__main__':
+   app.run(host='0.0.0.0',port=8080)
